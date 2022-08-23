43050dfcd28f44942859d68b35d5f990907a3184
shariful4345 <mdsajeeb4345@gmail.com>
2022-08-23 16:40:27 +0600

    added all file

---
+++ js/custom.js
@@ -0,0 +1,62 @@
+
+const selectButtons = document.getElementsByClassName("selectBtn");
+const playerList = document.getElementById("playerList");
+let count = 0;
+const calculate = document.getElementById("Calculate-btn"); // 6
+const playerExpense = document.getElementById("playerExpense");
+const total = document.getElementById("total-Amount");
+const calculateTotal = document.getElementById("calculat-total-btn");
+
+for (const button of selectButtons) {
+    button.addEventListener("click", function (event) {
+        if (count < 5) {
+            const getTheButton = event.target.parentNode;
+            const playerName =
+                getTheButton.querySelector(".playerName").innerText;
+            count++;
+
+            const theLi = document.createElement("li");
+            const theList = playerList.appendChild(theLi);
+            theList.innerHTML = `
+                     <span class="listItem">${count}.</span>${playerName}
+                     `;
+            button.disabled = true;
+            button.innerText = "SELECTED";
+            if (button.disabled === true) {
+                button.style.backgroundColor = "#A2A9AF";
+            }
+        } else {
+            alert("You Selected the maximum Number of players");
+        }
+    });
+}
+
+calculate.addEventListener("click", function () {
+    // Get Coast of per Player
+    const perPlayerCoast = getInput("player-input-fild");
+    const totalExpense = perPlayerCoast * count;
+
+    // added Player Expense
+    playerExpense.innerText = totalExpense;
+
+    // const theChildren = playerList.children;
+    // console.log(theChildren.length);
+});
+
+calculateTotal.addEventListener("click", function () {
+    const playerExpenses = parseFloat(playerExpense.innerText);
+    const managerCoast = getInput("manager-input-fild");
+    const coachCoast = getInput("coach-input-fild");
+
+    // Added Total coast
+    const totalCoast = playerExpenses + managerCoast + coachCoast;
+    total.innerText = totalCoast;
+});
+
+function getInput(element) {
+    const input = document.getElementById(element);
+    const inputString = input.value;
+    const inputValueNumber = parseFloat(inputString);
+    input.value = "";
+    return inputValueNumber;
+}
